a1b4f2a61e55fdf5bc043567de5a7d11c4a4b6e9
Alexey <saakyanag@gmail.com>
2021-12-14 12:16:16 +0300

    Edit docs

--- Project_0 Game Number Guess/game.py
+++ Project_0 Game Number Guess/game.py
@@ -1,5 +1,4 @@
-"""
-    УСЛОВИЯ ЗАДАНИЯ
+"""УСЛОВИЯ ЗАДАНИЯ.
 
 1. Компьютер загадывает целое число от 1 до 100, и нам его нужно угадать. Под «угадать» подразумевается «написать программу, которая угадывает число».
 2. Алгоритм учитывает информацию о том, больше ли или меньше случайное число нужного нам числа.
@@ -7,14 +6,13 @@
 МЕТРИКА КАЧЕСТВА
 
 1. Результаты оцениваются по среднему количеству попыток при 1000 повторений. Необходимо добиться минимального количества попыток.
-
-    """
+"""
 
 
 import numpy as np
 
 
-def think_number(up_border: int = 100) -> int: 
+def think_number(up_border: int = 100) -> int:
     """Функция загадывает целое число от 1 до верхней границы.
 
     Args:
@@ -26,15 +24,15 @@ def think_number(up_border: int = 100) -> int:
     Returns:
         int: Возвращает загаданное число.
     """
-    
-    if type(up_border) is int: # Проверим на корректность ввод границы
-        return np.random.randint(1,up_border + 1)
-    
+
+    if (type(up_border) is int) and (up_border > 10):  # Проверим на корректность ввод границы
+        return np.random.randint(1, up_border + 1)
+
     else:
         raise ValueError('Invalid border type. It must be integer!')
 
 
-def number_guess(thnk_num: int, up_border:int = 100, ptr_text: bool = True) -> int:
+def number_guess(thnk_num: int, up_border: int = 100, ptr_text: bool = True) -> int:
     """Функция угадывания числа.
 
     Args:
@@ -45,31 +43,32 @@ def number_guess(thnk_num: int, up_border:int = 100, ptr_text: bool = True) -> i
         int: Возвращает количество попыток, за которое угадано число.
     """
     # Зададим начальные параметры и сделаем первую попытку
-    count = 1 # Число попыток
-    min_num = 1 # Нижняя граница угадывания
-    max_num = up_border + 1 # Верхняя граница угадывания
-    guess_num = np.random.randint(min_num,max_num) # Делаем первую попытку
-    
-    while True: # Цикл угадывания со смещением верхней и нижней границы 
-        
+    count = 1  # Число попыток
+    min_num = 1  # Нижняя граница угадывания
+    max_num = up_border + 1  # Верхняя граница угадывания
+    guess_num = np.random.randint(min_num, max_num)  # Делаем первую попытку
+
+    while True:  # Цикл угадывания со смещением верхней и нижней границы
+
         if guess_num < thnk_num:
             min_num = guess_num
-            
+
         elif guess_num > thnk_num:
             max_num = guess_num
-        
-        else:
-            if ptr_text: # Проверка нужно ли печатать результаты
-                print(f'Число {thnk_num} угадано за {count} попыток.')
-        
-            return count
-        
-        count += 1
-        guess_num = np.random.randint(min_num,max_num)
-        
 
-def mean_score(up_border:int = 100, prt_text: bool = True) -> int:
-    """Функция считает число попыток 1000 раз и возвращает среднее число попыток.
+        else:
+            if ptr_text:  # Проверка нужно ли печатать результаты
+                print(f'Число {thnk_num} угадано за {count} попыток.')
+
+            return count
+
+        count += 1
+        guess_num = np.random.randint(min_num, max_num)
+
+
+def mean_score(up_border: int = 100, prt_text: bool = True) -> int:
+    """Функция считает число попыток 1000 раз и возвращает среднее число
+    попыток.
 
     Args:
         up_border (int, optional): Верхняя граница даипозона угадывания. Defaults to 100.
@@ -78,19 +77,20 @@ def mean_score(up_border:int = 100, prt_text: bool = True) -> int:
     Returns:
         int: Возвращает среденее число попыток из 1000 игр.
     """
-    
-    count_ls = [] # Задаем начальное значение списка попыток
-    
-    for i in range(999): # Делаем все 1000 попыток
-        count_ls.append(number_guess(think_number(up_border),up_border, False))
-    
-    score = int(np.mean(count_ls)) # Считаем результаты
-    
-    if prt_text: # Проверка нужно ли печатать результаты
-        print(f'Среднее число попыток для даипозона от 1 до {up_border} - {score}.')
-        
+
+    count_ls = []  # Задаем начальное значение списка попыток
+
+    for i in range(999):  # Делаем все 1000 попыток
+        count_ls.append(number_guess(
+            think_number(up_border), up_border, False))
+
+    score = int(np.mean(count_ls))  # Считаем результаты
+
+    if prt_text:  # Проверка нужно ли печатать результаты
+        print(
+            f'Среднее число попыток для даипозона от 1 до {up_border} - {score}.')
+
     return score
-        
 
 
 if __name__ == '__main__':
@@ -98,4 +98,4 @@ if __name__ == '__main__':
     print(mean_score(50001, False))
     mean_score()
     mean_score(1000)
-    print(mean_score(1000, False))   
+    print(mean_score(1000, False))
